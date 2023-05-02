2ac1b0645f5abdf8a508cb928c39a94ab97db3a9
iMaxZorky <117920937+iMaxZorky@users.noreply.github.com>
2023-05-02 08:28:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import Keyboard from './src/keyboard-class.js';
 
+alert('Дико извиняюсь, что не заметил что надо делать деплой pull request-а');
+alert('Вот ссылка на него: https://github.com/iMaxZorky/virtual-keyboard/pull/1');
+
 document.body.prepend(new Keyboard().main);
